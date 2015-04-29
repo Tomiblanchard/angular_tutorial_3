4960ab72173dfc05b197863f5f16904e095bdf66
M1lena <stoevami@gmail.com>
2015-04-29 12:20:36 +0100

    got github token to work

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,6 +1,8 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 
   var self = this;
+
+  var searchResource = $resource('https://api.github.com/search/users?access_token=d6cc97081f07efeddade53ebeca259cfae57d754');
 
   // self.searchResult = {
   //   "items": [
@@ -18,20 +20,8 @@ githubUserSearch.controller('GitUserSearchController', [function() {
   // };
 
   self.doSearch = function() {
-    self.searchResult = {
-      items: [
-        {
-          "login": "tansaku",
-          "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-          "html_url": "https://github.com/tansaku"
-        },
-        {
-          "login": "stephenlloyd",
-          "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-          "html_url": "https://github.com/stephenlloyd"
-        }
-      ]
-    }
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
   };
-
 }]);
