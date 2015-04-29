2de990bae4c77f8ac39e4cfa90301f663fd7a038
M1lena <stoevami@gmail.com>
2015-04-29 11:07:07 +0100

    git user search controler initialise search test passes

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -5,10 +5,10 @@ describe('GitUserSearchController', function() {
 
   beforeEach(inject(function($controller) {
     ctrl = $controller('GitUserSearchController');
+  }));
 
     it('initialises with an empty search result and term', function() {
       expect(ctrl.searchResult).toBeUndefined();
       expect(ctrl.searchTerm).toBeUndefined();
     });
-  }));
 });
